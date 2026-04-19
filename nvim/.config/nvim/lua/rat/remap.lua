c40212448ca12535bb7b35818ea90c9cebc68e67
ayoobf <rflooivl@gmail.com>
2026-04-18 22:25:01 -0400

    removed nvim config. use nvim.kickstart

--- nvim/.config/nvim/lua/rat/remap.lua
+++
@@ -1,3 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.diagnostic.config({ virtual_text = { enabled = true } })
--- nvim/.config/nvim/plugin/init.lua
+++
@@ -1,5 +0,0 @@
-require("rat.remap")
-vim.o.relativenumber = true
-vim.o.number = true
-vim.opt["tabstop"] = 4
-vim.opt["shiftwidth"] = 4
